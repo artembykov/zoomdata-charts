99a79c0bd23cd9ac62e184fb767969f49c0388aa
Artem Bykov <artemio91@gmail.com>
2018-10-19 17:03:11 +0300

    Updates

---
+++ precache-manifest.001a00be7eaaf33c6e50589ff7e71449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zoomdata-charts/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4077537a0f50f79c7a45",
+    "url": "/zoomdata-charts/static/js/runtime~main.4077537a.js"
+  },
+  {
+    "revision": "241e4008aa7278572839",
+    "url": "/zoomdata-charts/static/js/main.241e4008.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/zoomdata-charts/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "241e4008aa7278572839",
+    "url": "/zoomdata-charts/static/css/main.1cdc5e99.chunk.css"
+  },
+  {
+    "revision": "621643e12352670db12e3581280ef621",
+    "url": "/zoomdata-charts/index.html"
+  }
+];
